90f51d97409d87b598386f2ca82e6e3168478d32
Florent Messa <florent.messa@gmail.com>
2011-02-19 09:47:08 +0100

    URL is required

--- social_bookmarking/urls.py
+++ social_bookmarking/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls.defaults import *
 from social_bookmarking.views import bookmark_referer
 
 urlpatterns = patterns('', 
-    url(r'^referer/(?P<slug>[-\w]+)/(?P<content_type>[\.\w]+)/(?P<object_pk>[\d]+)/(?P<url>.*)$',
+    url(r'^referer/(?P<slug>[-\w]+)/(?P<content_type>[\.\w]+)/(?P<object_pk>[\d]+)/(?P<url>.+)$',
         bookmark_referer,
         name='bookmark_referer'),
 )
